be415c9ce4d574a8da2b1cc2ea685d75aae8b292
naseroish <82692921+naseroish@users.noreply.github.com>
2025-10-01 15:30:44 +0300

    Update Azure SQL Database max size to 2GB for Basic tier support

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_mssql_database" "main" {
   server_id    = azurerm_mssql_server.main.id
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
-  max_size_gb  = 20
+  max_size_gb  = 2  # Basic tier supports up to 2GB
   sku_name     = "Basic"
   
   tags = var.tags
